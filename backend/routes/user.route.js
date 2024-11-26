3ffb84ca4aaf851647f70d62f3d1b31dc09f951b
Jem Centino <138832632+Kuhwu@users.noreply.github.com>
2024-11-26 21:26:25 +0800

    Backend Temporary Changes
    Temporary Function and Routing (Sign in & Sign out)

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,50 @@
-const express = require('express')
+const User = require('../models/user.model')
 
+const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) =>{
-    console.log('Hello World')
+router.post('/signup', (req,res)=>{
+    User.findOne({email:req.body.email}, (err,user)=>{
+        if(err){
+            console.log(err)
+            res.json(err)
+        }else{
+            if(user==null){
+                const user = User({
+                    username:req.body.username,
+                    email:req.body.email,
+                    password:req.body.password,
+                })
+                user.save()
+                .then((error)=>{
+                    if(err){
+                        console.log(err)
+                        res.json(err)
+                    }else{
+                        console.log(user)
+                        res.json(user)
+                    }
+                    console.log(user)
+                })            
+            }else{
+                res.json({
+                    message: 'Email is Already Registered'
+                })
+            }
+        }
+    })
 })
+
+router.post('/signin', (req,res)=>{
+    User.findOne({email:req.body.email,password:req.body.password}, (err,user)=>{
+        if(err){
+            console.log(err)
+            res.json(err)
+        }else{
+            res.json(user)
+        }
+    })
+})
+
 
 module.exports = router
