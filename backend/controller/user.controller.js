17d831be2d44fff76f454fb85f8f9e060e3c9858
Jem Centino <138832632+Kuhwu@users.noreply.github.com>
2024-11-30 18:27:37 +0800

    Backend Bug Fixed
    Fixed Login bug, the password stored in the database is hashed using bcrypt, and it won't match with the raw password from the reset request

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -35,12 +35,23 @@ const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email, password });
+        const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        return res.status(200).json(user);
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        return res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email
+            }
+        });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal Server Error' });
