ff4883faeea1ea0adb9d2d44860d81694430036b
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-12-08 23:15:57 +0800

    lazy commit

---
+++ frontend/lib/maharani/screens/authScreens/sigun_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SigunScreen extends StatelessWidget {
+  const SigunScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
