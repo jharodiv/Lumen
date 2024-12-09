c4d62a9d2b04cb2535edcab2b27f61e9da30c0aa
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-12-09 21:49:08 +0800

    lazy commit pt.2

---
+++ frontend/lib/maharani/widgets/authentication/signup_form.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 3.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 80,
+                  child: Stack(
+                    children: [
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black, width: 3.0),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: -13,
+                        right: 0,
+                        child: TextButton(
+                          onPressed: () {
+                            //GoToNextScreen
+                          },
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 57, 153, 107),
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                    textStyle: const TextStyle(fontSize:20),
+                  ),
+                  child: const Text('Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account?",
+                    style: TextStyle(
+                      fontSize:13,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    ),
+                    TextButton(
+                      onPressed: (){
+                        //GoToSignUpScreen
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize:13,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
