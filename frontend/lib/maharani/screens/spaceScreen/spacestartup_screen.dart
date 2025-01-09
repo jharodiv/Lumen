02b78f6e7e6c2c39a3d8521d1ffedeb49e5aac6b
Jem Centino <138832632+Kuhwu@users.noreply.github.com>
2025-01-09 15:05:39 +0800

    Create Space Widget UI

--- frontend/lib/maharani/screens/spaceScreen/spacestartup_screen.dart
+++ frontend/lib/maharani/screens/spaceScreen/spacestartup_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/maharani/widgets/myspaceStartup/createstartup.dart';
+import 'package:frontend/maharani/widgets/myspaceStartup/joinstartup.dart';
 
 class JoinSpaceStartUp extends StatelessWidget {
   const JoinSpaceStartUp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          // JoinStartup widget at the top
+          Expanded(
+            flex: 1,
+            child: Joinstartup(),
+          ),
+          // Createstartup widget at the bottom
+          Expanded(
+            flex: 1,
+            child: Createstartup(),
+          ),
+        ],
+      ),
+    );
   }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/maharani/screens/splashscreen.dart';
-import 'package:frontend/maharani/widgets/myspaceStartup/joinstartup.dart';
-//import 'package:frontend/maharani/screens/authScreens/signup_screen.dart';
+//import 'package:frontend/maharani/screens/splashscreen.dart';
+import 'package:frontend/maharani/widgets/myspaceStartup/createstartup.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Joinstartup(),
+    home: Createstartup(),
   ));
 }
