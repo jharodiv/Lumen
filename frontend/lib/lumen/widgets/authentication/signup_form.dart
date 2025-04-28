30d1016839e669913bc3a2169fed84439c8b5e90
Kawu <138832632+kawudiv@users.noreply.github.com>
2025-04-28 17:45:40 +0800

    SignUp Form New Design Done

--- frontend/lib/lumen/widgets/authentication/signup_form.dart
+++ frontend/lib/lumen/widgets/authentication/signup_form.dart
@@ -8,90 +8,164 @@ class SignupForm extends StatelessWidget {
     final TextEditingController emailController = TextEditingController();
 
     return Center(
-        child: SizedBox(
-      width: MediaQuery.of(context).size.width * 0.8,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'Sign Up',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                fontFamily: 'Inter',
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Row(
+                children: [
+                  Text(
+                    'Lumen',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      fontFamily: 'Inter',
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 30),
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email Address',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 5),
+              // Left-aligned text below 'Lumen'
+              const Row(
+                children: [
+                  Expanded(
+                    // This makes sure the text takes up available space and wraps when necessary.
+                    child: Text(
+                      'Capture the moments, share the memories, and create your story together.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
+                        fontFamily: 'Inter',
+                      ),
+                      softWrap:
+                          true, // Ensures the text wraps onto the next line
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                final email = emailController.text.trim();
-                if (email.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please Enter your email')),
-                  );
-                  return;
-                }
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                  content: Text('Continue with email'),
-                ));
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 57, 153, 107),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 18),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  )),
-              child: const Text(
-                'Continue',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+
+              const SizedBox(height: 30),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email address',
+                  border: OutlineInputBorder(),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () {
-                //ForFacebookLogin
-              },
-              icon: const Icon(Icons.facebook, color: Colors.white),
-              label: const Text('Continue with facebook'),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue[800],
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  final email = emailController.text.trim();
+                  if (email.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please Enter your email')),
+                    );
+                    return;
+                  }
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('Continue with email'),
+                  ));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 57, 153, 107),
                   minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 18),
+                  textStyle: const TextStyle(fontSize: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
-                  )),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton.icon(
-              onPressed: () {
-                //GoogleLoginFunction
-              },
-              icon: const Icon(Icons.g_mobiledata, color: Colors.white),
-              label: const Text('Continue with Google'),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red[600],
+                  ),
+                ),
+                child: const Text(
+                  'Continue',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Facebook login function
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+                  foregroundColor: Colors.white,
                   minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 18),
+                  side: BorderSide(
+                    color: Colors.white.withOpacity(0.5),
+                    width: 1,
+                  ),
+                  textStyle: const TextStyle(fontSize: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
-                  )),
-            ),
-          ],
+                  ),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset(
+                        'assets/images/authentication/facebook.png',
+                        height: 24,
+                        width: 24,
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        'Continue with Facebook',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Google login function
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  side: BorderSide(
+                    color: Colors.white.withOpacity(0.5),
+                    width: 1,
+                  ),
+                  textStyle: const TextStyle(fontSize: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset(
+                        'assets/images/authentication/google.png',
+                        height: 24,
+                        width: 24,
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        'Continue with Google',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Inter',
         scaffoldBackgroundColor: const Color(0xFF191818),
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF191818),
