bd4350305cc80c503ec411551154cb597f7a342e
Kawu <138832632+kawudiv@users.noreply.github.com>
2025-04-29 17:29:40 +0800

    Authentication Image Done
    devices responsiveness not yet tested

--- frontend/lib/lumen/widgets/authentication/authentication_image.dart
+++ frontend/lib/lumen/widgets/authentication/authentication_image.dart
@@ -6,32 +6,67 @@ class AuthenticationImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    double itemSpacing = 12.0;
 
     return Container(
-      width: screenWidth,
-      padding: const EdgeInsets.all(16),
-      child: GridView.count(
-        shrinkWrap: true,
-        crossAxisCount: 2,
-        mainAxisSpacing: 12,
-        crossAxisSpacing: 12,
-        childAspectRatio: 1.2,
-        physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(20),
+      child: Wrap(
+        spacing: itemSpacing,
+        runSpacing: itemSpacing,
         children: [
-          _buildImage('assets/images/authentication/image1.jpg'),
-          _buildImage('assets/images/authentication/image2.jpg'),
-          _buildImage('assets/images/authentication/image3.jpg'),
-          _buildImage('assets/images/authentication/image4.jpg'),
+          _buildTranslatedImageBox(
+            'assets/images/authentication/image1.jpg',
+            width: screenWidth * 0.43,
+            height: screenWidth * 0.3,
+            offset: Offset(screenWidth * -0.08, screenHeight * 0.02),
+          ),
+          _buildTranslatedImageBox(
+            'assets/images/authentication/image2.jpg',
+            width: screenWidth * 0.43,
+            height: screenWidth * 0.3,
+            offset: Offset(screenWidth * 0.03, screenHeight * 0.01),
+          ),
+          _buildTranslatedImageBox(
+            'assets/images/authentication/image3.jpg',
+            width: screenWidth * 0.43,
+            height: screenWidth * 0.3,
+            offset: Offset(-screenWidth * -0.01, -screenHeight * -0.03),
+          ),
+          _buildTranslatedImageBox(
+            'assets/images/authentication/image4.jpg',
+            width: screenWidth * 0.43,
+            height: screenWidth * 0.3,
+            offset: Offset(screenWidth * 0.10, screenHeight * 0.02),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildImage(String path) {
+  Widget _buildTranslatedImageBox(
+    String path, {
+    required double width,
+    required double height,
+    required Offset offset,
+  }) {
+    return Transform.translate(
+      offset: offset,
+      child: _buildImageBox(path, width: width, height: height),
+    );
+  }
+
+  Widget _buildImageBox(
+    String path, {
+    required double width,
+    required double height,
+  }) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(16),
       child: Image.asset(
         path,
+        width: width,
+        height: height,
         fit: BoxFit.cover,
       ),
     );
